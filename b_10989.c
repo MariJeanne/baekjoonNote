e650d3ca7594bcc45298bcbf7a99162ad2f0f60c
MariJeanne <53007682+MariJeanne@users.noreply.github.com>
2020-01-18 11:24:23 +0900

    Update b_10989.c
    change dynamic alloc, to resolve memory overflow
    (FAIL)

--- b_10989.c
+++ b_10989.c
@@ -1,27 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
-#define MAX_SIZE 10000001
+#define MAX_SIZE 10000
+#define MAX_NUM 10000
 #define SWAP(x,y,t) (t)=(x); (x)=(y); (y)=(t);
 
 int main(){
-    int t, i, max=0, temp, n;
-    int IN[MAX_SIZE], OUT[MAX_SIZE];
-    int *CNT;
+    int t, i, temp, max=0;
+    int *IN, *OUT, *CNT;
     scanf("%d", &t);
+    IN = (int*)malloc(sizeof(int)*t);
+    OUT = (int*)malloc(sizeof(int)*t);
     for(i=0; i<t; i++) {
         scanf("%d", &IN[i]);
-        if(IN[i]>max) max = IN[i];
+        if(IN[i] > max) max = IN[i];
     }
     max++;
     CNT = (int*)malloc(sizeof(int)*max);
-    for(i=0; i<max; i++) CNT[i] = 0;
     for(i=0; i<t; i++) CNT[IN[i]] += 1;
     for(i=1; i<max; i++) CNT[i] = CNT[i-1] + CNT[i];
-    
-    for(i=t; i>=0; i--){
-        n = IN[i];
-		SWAP(IN[CNT[n]], IN[i], temp);
-		CNT[n]--;
-	}
-	for(i=1; i<=t; i++) printf("%d ", IN[i]);
+    for(i=1; i<max; i++) printf("<%d> ", CNT[i]);
+
+    for(i=t-1; i>=0; i--){
+        OUT[CNT[IN[i]]] = IN[i];
+        CNT[IN[i]]--;
+    }
+	for(i=1; i<=t; i++) printf("%d ", OUT[i]);
 }
