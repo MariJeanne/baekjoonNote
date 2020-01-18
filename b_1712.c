98a94efa053ff26ea8122a9dbd389324dafc9ec9
MariJeanne <53007682+MariJeanne@users.noreply.github.com>
2020-01-18 14:07:26 +0900

    retry baekjoon no.1712
    case bef 0 is error case too, so that case return -1

--- b_1712.c
+++ b_1712.c
@@ -3,11 +3,12 @@ int main(){
     int a, b, c, i, bef;
     scanf("%d %d %d", &a, &b, &c);
     
-    if(b==c) bef = -1;
-    else{ 
-        for(i=0;;i++) if(c*i>(a+b*i)) break;
-        bef = i;
+    if(b>=c) bef = -1;
+    else {
+        bef = a / (c-b) + 1;
+        if(bef<0) bef = -1;
     }
+    
     printf("%d", bef);
     return 0;
 }
