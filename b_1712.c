b2004011a620633759664137b9fe7adc5486e7d9
MariJeanne <53007682+MariJeanne@users.noreply.github.com>
2020-01-18 13:44:18 +0900

    retry baekjoon no.1712
    edit mistake
    but FAILURE

--- b_1712.c
+++ b_1712.c
@@ -3,12 +3,11 @@ int main(){
     int a, b, c, i, bef;
     scanf("%d %d %d", &a, &b, &c);
     
-    if(b>c || a==0) bef = 0;
-    else if(b==c) bef = -1;
+    if(b==c) bef = -1;
     else{ 
-        for(i=1;;i++) if(c*i>(a+b*i)) break;
+        for(i=0;;i++) if(c*i>(a+b*i)) break;
         bef = i;
     }
-    printf("%d", i);
+    printf("%d", bef);
     return 0;
 }
