c7a9fe35faf6f3fa9c9ce8d7d912acf419e64e46
MariJeanne <53007682+MariJeanne@users.noreply.github.com>
2020-01-20 18:11:43 +0900

    retry baekjoon no.1463
    make recursive function to check all cases
    FAILURE - time overflow

--- b_1463.c
+++ b_1463.c
@@ -1,12 +1,26 @@
 #include <stdio.h>
+#include <stdlib.h>
+int cal(int);
+
 int main(){
-    int n, cnt=0;
+    int n;
     scanf("%d", &n);
-    while(n!=1){
-        if(n%3==0) n/=3;
-        else if(n%2==0) n/=2;
-        else n-=1;
-        cnt++;
+    printf("%d", cal(n));
+}
+
+int cal(int num){
+    int cals[3];
+    int i, min=num;
+    
+    if(num==1) return 0;
+    else{
+        cals[0] = 1 + cal(num-1);
+        if(num%2==0) cals[1] = 1 + cal(num/2);
+        else cals[1] = num;
+        if(num%3==0) cals[2] = 1 + cal(num/3);
+        else cals[2] = num;
+        
+        for(i=0; i<3; i++) if(cals[i]<min) min = cals[i];
+        return min;
     }
-    printf("%d", cnt);
 }
